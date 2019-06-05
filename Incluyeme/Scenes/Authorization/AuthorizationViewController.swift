9dd47ad96fe336097d95db476a067a72594b426b
Daniel Aragon <daniel.aragon.0235@hotmail.com>
2019-06-04 19:49:21 -0500

    internet checked

--- Incluyeme/Scenes/Authorization/AuthorizationViewController.swift
+++ Incluyeme/Scenes/Authorization/AuthorizationViewController.swift
@@ -12,18 +12,22 @@ class AuthorizationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+    @IBAction func proceedAction(_ sender: Any) {
         if (!Utils.checkInternetConnection()){
             let viewController = self.storyboard!.instantiateViewController(withIdentifier: "InternetAlertViewController")
             viewController.definesPresentationContext = true
             viewController.modalPresentationStyle = UIModalPresentationStyle.overFullScreen
             viewController.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
-            self.present(viewController,animated: true, completion: nil)
+            self.present(viewController,animated: true, completion: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5), execute: {
+                    exit(0)
+                })
+            })
+        }else{
+            performSegue(withIdentifier: "HomeSegue", sender: nil)
         }
-    }
-    
-    @IBAction func proceedAction(_ sender: Any) {
-        performSegue(withIdentifier: "HomeSegue", sender: nil)
     }
     
 }
